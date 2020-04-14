1f8dd44dd9c7b3a1de2918075b6b1eea175dc809
SuYOUNGgg <63627291+SuYOUNGgg@users.noreply.github.com>
2020-04-14 16:44:36 +0900

    TEST_WITH_THEO

---
+++ Test.java
@@ -0,0 +1,8 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+	System.out.println("Welcome to Java");
+	System.out.println("First Java program");
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,13 @@
+// Test2.java
+// javac Test2.java
+// java Test2
+class Test2
+{
+
+	 public static void main(String[] args)
+	{
+	System.out.println("Hello Java");
+	System.out.println("Good day");
+	}
+
+}
