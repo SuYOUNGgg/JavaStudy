51aeabd4d3eb30f3f6facbf578c7e3c58879e0a0
ILJU-MUN <iljum9072@naver.com>
2020-05-11 12:58:15 +0900

    database

--- Database/0508/workshop3~1.sql
+++ Database/0508/workshop3~1.sql
@@ -1,34 +1,33 @@
 //Q1
 --주문번호 ‘19971213’에 대하여 주문번호, 주문순번, 제품번호, 주문수량을 주문수량의 오름차순으로 조회하세요
-select a.ord_no "ORD_NO", b.line_no "LINE_NO", b.item_no "ITEM_",b.ord_qty "ORD_QTY"
-from po_order_header a, po_order_detail b, po_item c
-where a.ord_no = '19971213'
-and a.ord_no=b.ord_no
-and b.item_no=c.item_no
-order by b.ord_qty;
+select ord_no , line_no ,item_no ,ord_qty 
+from po_order_detail
+where ord_no = '19971213'
+order by ord_qty;
 
 //Q2
 --1997년 12월에 공급처 ‘LG전자’로 주문한 모든 건에 대해 주문번호와 주문일 (YYYY-MM-DD 형식으로) 을 주문 일의 내림차순으로 조회하세요
 select a.ord_no, to_char(a.ord_date,'yyyy-mm-dd')
 from po_order_header a, po_supplier b
 where a.sup_no=b.sup_no
-and b.sup_name like 'LG%'
+and b.sup_name = 'LG전자'
 and to_char(a.ord_date,'yyyymm') =199712
 order by a.ord_date desc; 
 
 //Q3
 --주문번호가 ‘19971203’인 주문에 대해 주문번호, 제품번호, 제품명, 제품별 주문수량 을 제품번호의 오름차순으로 조회하세요. 
-select a.ord_no,b.item_no,c.item_name,b.ord_qty
-from po_order_header a, po_order_detail b, po_item c
-where a.ord_no = b.ord_no
-and b.item_no=c.item_no
-and a.ord_no ='19971203';
+select a.ord_no,a.item_no,b.item_name,a.ord_qty
+from po_order_detail a, po_item b
+where a.item_no=b.item_no
+and a.ord_no ='19971203'
+order by a.item_no;
 
 //Q4
 --납기가 지났으나 납품이 완료되지 않은 모든 주문에 대하여 주문번호와 공급처 이름, 전화번호를 공급처 이름의 오름차순으로 조회하세요.
 select a.ord_no,b.sup_name,b.sup_phone
 from po_order_header a,po_supplier b
-where a.delv_date is null
+where to_char(a.due_date,'yyyymmdd') < to_char(sysdate,'yyyymmdd')
+and a.delv_date is null
 and a.sup_no=b.sup_no
 order by b.sup_name;
 
@@ -41,9 +40,10 @@ order by emp_dep;
 
 //Q6
 --‘홍보부’의 ‘박찬호’ 직원이 1997년에 주문을 낸 공급처의 이름과 공급처의 주소를 공급처 이름의 오름차순으로 중복되지 않게 조회하세요.
-select a.sup_name, a.sup_addr
+select distinct a.sup_name, a.sup_addr
 from po_supplier a, po_employeer b, po_order_header c
 where b.emp_name = '박찬호'
+and b.emp_dep= '홍보부'
 and a.sup_no=c.sup_no
 and c.ord_emp=b.emp_no
 and to_char(c.ord_date,'yyyy')='1997';
@@ -54,19 +54,18 @@ select sum(a.unit_price*d.ord_qty) "총 지불 대금"
 from po_item a, po_order_header b, po_supplier c,po_order_detail d
 where b.ord_no = d.ord_no
 and d.item_no = a.item_no
-and to_char(b.delv_date,'yy/mm')='97/12'
+and to_char(b.delv_date,'yyyymm')='199712'
 and c.sup_no=b.sup_no
 and c.sup_name='LG전자';
 
 
 //Q8
 --납기를 잘 지키지 않는 불량 공급자를 파악하기 위해 공급자별로 납기 미 준수 횟수( 납품이 아직 안된 것은 제외) 가 2건 이상인 공급처를 공급처 번호와 납기 미준수 횟수로 조회하세요. 
-select b.sup_no "공급처 번호",count(a.sup_no) "납기 미준수 횟수"
-from po_order_header a,po_supplier b
-where  a.sup_no=b.sup_no
-and a.due_date<a.delv_date
-group by b.sup_no
-having count(a.sup_no)>=2 ;
+select sup_no "공급처 번호",count(sup_no) "납기 미준수 횟수"
+from po_order_header
+where due_date<delv_date
+group by sup_no
+having count(sup_no)>=2 ;
 
 //Q9
 --1997년에 주문한 제품의 단가 중에 가장 높은 단가를 조회하세요.
